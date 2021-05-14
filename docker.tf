e3d73a40adf3055e83bff1a72b4200e853e7d4ba
AndersonLoyem <30845852+Andyloyem@users.noreply.github.com>
2021-05-14 08:12:39 +0200

    Update docker.tf

--- docker.tf
+++ docker.tf
@@ -1,1 +1,14 @@
-test
+# Configure the Docker provider
+provider "docker" {
+  host = "tcp://127.0.0.1:5000/"
+}
+
+# Create a container
+resource "docker_container" "api" {
+  image = docker_image.student-list_api
+  name  = "api"
+}
+
+resource "docker_image" "student-list_api" {
+  name = "student-list_api"
+}
